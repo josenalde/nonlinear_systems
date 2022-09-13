84a305262a7fb4f6ea499600c032a229a8092dc5
josenaldeOliveira <josenalde@gmail.com>
2022-09-12 23:26:26 -0300

    Add files via upload

--- scripts/phase_portrait_1.m
+++ scripts/phase_portrait_1.m
@@ -8,8 +8,13 @@ clear all; close all; clc
 #y" + sin(y) = 0
 #Fazendo x_1 = y e x_2 = y', tem-se x_1' = x_2 e x_2'=-sin(y)
 
-#Função lambda
-f_1 = @(t,X) [X(2); -sin(X(1))];
+#Função lambda sem atrito
+#f_1 = @(t,X) [X(2); -sin(X(1))];
+
+#Função lambda comet atrito
+f_3 = @(t,X) [X(2); -sin(X(1)) - 0.5*X(2)]; #g=l=10,m=1,B=0.5
+
+f_4 = @(t,X) [X(2); -2*X(1) - X(1)^2 - 0.5*X(2)];
 
 #Exemplo 2: VAN DER POL
 #mx" + 2c(x^2-1)x' + kx=0
@@ -17,10 +22,10 @@ m=1; c=0.3; k=1; #testar para c variando 0.1, 0.2, 0.5, 1.0, 1.5, 2.0
 f_2 = @(t, X) [X(2); (1/m)*(-2*c*(X(1)^2-1)*X(2)-k*X(1))];
 
 #Vamos definir o intervalo em x_1 e x_2 de interesse
-x_1_min = -3; #-2,8,-2,2
-x_1_max = 3;
-x_2_min = -4;
-x_2_max = 4;
+x_1_min = -10; #-2,8,-2,2
+x_1_max = 10;
+x_2_min = -10;
+x_2_max = 10;
 
 #cria eixos
 x_1 = linspace(x_1_min, x_1_max, 20);
@@ -34,8 +39,8 @@ v = zeros(size(x));
 
 t=0;
 for i = 1:numel(x)
-    Xprime = f_2(t,[x(i); x_dot(i)]);
-    #Xprime = f_1(t,[x(i); x_dot(i)]);
+    #Xprime = f_2(t,[x(i); x_dot(i)]);
+    Xprime = f_4(t,[x(i); x_dot(i)]);
     u(i) = Xprime(1);
     v(i) = Xprime(2);
 end
@@ -47,8 +52,9 @@ axis tight equal;
 
 #Algumas trajetórias
 hold on
-for x_2_0 = [0 0.5 1 1.5 2 2.5]
-    [ts,xs] = ode45(f_2,[0,50],[0;x_2_0]);
+#for x_2_0 = [1]
+for x_2_0 = [0.5 1 1.5 2.5 4.5]
+    [ts,xs] = ode45(f_4,[0,50],[0;x_2_0]);
     plot(xs(:,1),xs(:,2))
     plot(xs(1,1),xs(1,2),'bo') % starting point
     plot(xs(end,1),xs(end,2),'ks') % ending point
